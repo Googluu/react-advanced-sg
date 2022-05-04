576ed2127ceba0c53ba14a38f7da7e635c912f0c
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-05-03 23:01:06 -0500

    interection-component

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,27 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { Category } from '../Category';
+import React, { Fragment, useEffect, useState } from 'react'
+import { Category } from '../Category'
 
-import { List, Item } from './styles';
+import { List, Item } from './styles'
 
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(function () {
+    setLoading(true)
+    window.fetch('https://petgram-server-leidy-daza-leidydaza.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+
+  return { categories, loading }
+}
 
 export const ListOfCategories = () => {
-    const [ categories, setCategories ] = useState([])
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
-    useEffect(function () {
-        fetch('https://petgram-server.midudev.vercel.sh/categories')
-        .then(res => res.json())
-        .then(response => {
-            setCategories(response)
-        })
-    })
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
 
-    return (
-        <List>
-            {
-                categories.map(category => <Item key={category.id}>
-                    <Category {...category} />
-                </Item>)
-            }
-        </List>
-    )
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+  )
 }
